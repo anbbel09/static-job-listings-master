e80dfbc0d47b5a4ceece6f0b800aa31fe851b546
magi <magali.gonzalez0908@gmail.com>
2025-04-17 06:32:45 -0300

    responsive tablet

--- index.js
+++ index.js
@@ -144,21 +144,21 @@ fetch('data.json')
         const tags = [job.role, job.level, ...job.languages, ...job.tools];
 
         searchContainer.innerHTML += `
-          <article data-id="${job.id}" class="rounded-lg py-1 px-4 relative bg-white w-full h-auto mb-15 shadow-lg job cursor-pointer">
-            <img class="w-1/6 -top-[28px] absolute" src="${job.logo}" alt="${job.company} logo">
+          <article data-id="${job.id}" class="rounded-lg py-1 px-4 relative bg-white w-full h-auto mb-15 shadow-lg job cursor-pointer md:py-5 md:w-5/6 m-auto">
+            <img class="w-1/6 -top-[28px] absolute md:max-w-20 md:-top-[40px]" src="${job.logo}" alt="${job.company} logo">
             <div class="mt-8 flex flex-col justify-between gap-2 h-auto">
       
               <div class="">
-                <b class="flex gap-3 text-xs items-center">
-                  <h3 class="text-[#5ba4a4] font-bold">${job.company}</h3>
+                <b class="flex gap-3 text-xs items-center md:text-lg">
+                  <h3 class="text-[#5ba4a4] font-bold ">${job.company}</h3>
                   ${job.new ? `<h3 class="bg-[#5ba4a4] p-1 px-2 rounded-full text-white">NEW!</h3>` : ''}
                   ${job.featured ? `<h3 class="bg-[#2c3a3a] p-1 px-2 text-white rounded-full">FEATURED</h3>` : ''}
                 </b>
               </div>
       
-              <h3 class="font-semibold">${job.position}</h3>
+              <h3 class="font-semibold md:text-lg">${job.position}</h3>
       
-              <ul class="flex gap-10 text-sm text-gray-500 cursor-pointer">
+              <ul class="flex gap-10 text-sm text-gray-500 cursor-pointer md:text-md">
                 <li>${job.postedAt}</li>
                 <li class="list-disc list-outside">${job.contract}</li>
                 <li class="list-disc list-outside">${job.location}</li>
@@ -166,7 +166,7 @@ fetch('data.json')
       
               <hr class="my-3 h-1">
       
-              <div class="w-full h-auto flex gap-4 flex-wrap mb-3">
+              <div class="w-full h-auto flex gap-4 flex-wrap mb-3 md:text-lg">
               ${tags.map(tag => `
                 <span class="rounded-md p-2 bg-[#effafa] font-semibold text-[#5ba4a4] cursor-pointer tag text-sm">
                   ${tag}
